665c9bd0b18762be8563b943fe6f16847a90b3b4
daikiff <53941098+daikiff@users.noreply.github.com>
2019-08-09 21:27:13 +0900

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ログイン画面</title>
+    <link rel="stylesheet" href="/login.css">
+  </head>
+  <body>
+    <h1>ログイン画面</h1>
+    <form action="" method="post">
+      <input type="email" name="email" placeholder="メールアドレス"><br>
+      <input type="password" name="password" placeholder="パスワード" maxlength="12"><br>
+      <input type="submit"  id="login" value="ログイン">
+    </form>
+  </body>
+</html>
